eb5f21ff92a15938ec798c150ff328c98db8607c
aboud20-meet <aboud20@meet.mit.edu>
2018-11-01 19:00:58 +0200

    db

--- databases.py
+++ databases.py
@@ -10,16 +10,35 @@ session = DBSession()
 
 # Write your functions to interact with the database here :
 
-def create_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
+def addproduct(name , price , haveit , quantity):
+	product_object = Product(name=name, price=price, haveit=haveit , quantity=quantity)
+	session.add(product_object)
+	session.commit()
 
-def update_product():
-  #TODO: complete the functions (you will need to change the function's inputs)
-  pass
 
-def delete_product(id):
-  pass
+#def create_product():
+  #TODO: complete the functions (you will need to change the function's inputs)
+ # pass
+
+def updateproduct(name , price , haveit):
+	product_object = session.query(Product).filter_by(name=name).first()
+	product_object.haveit=haveit
+	if price<=300:
+		product_object.price=price
+	else:
+		print("the price is too high!")
+	session.commit()
+
+
+def deleteproduct(id):
+	session.query(Product).filter_by(id=id).delete()
+	session.commit()
+
 
 def get_product(id):
-  pass
+	pass  
+
+
+# addproduct("shirt" , 100 , True , 50)
+deleteproduct(2)
+updateproduct("shirt" , 340, False)
--- model.py
+++ model.py
@@ -7,5 +7,9 @@ Base = declarative_base()
 
 # Write your classes here :
 class Product(Base):
-    # TODO: complete this class
-    pass
+   __tablename__ = 'realmadrid'
+   id = Column(Integer, primary_key=True)
+   name = Column(String)
+   price = Column(Integer)
+   haveit = Column(Boolean)
+   quantity = Column(Integer)
